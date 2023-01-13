d2eac916b90c074488ab410a63b6b1e2552d33c6
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2023-01-13 22:42:00 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,4 @@
-//! `inline-vbs` is a crate that allows you to embed VBScript code inside Rust code files. It uses
-//! the [Active Scripting](https://docs.microsoft.com/en-us/archive/msdn-magazine/2000/december/active-scripting-apis-add-powerful-custom-debugging-to-your-script-hosting-app) COM APIs to dynamically parse and execute (optionally, evaluate) code.
-//!
-//! ### Basic usage
-//! ```rust
-//! use inline_vbs::*;
-//!
-//! vbs! { On Error Resume Next } // tired of handling errors?
-//! vbs! { MsgBox "Hello, world!" }
-//! let language = "VBScript";
-//! assert_eq!(vbs_!['language & " Rocks!"], "VBScript Rocks!".into());
-//! ```
-//!
-//! Macros:
-//! * `vbs!` - Executes a statement
-//! * `vbs_!` - Evaluates an expression
-//! * `vbs_raw!` - Executes a statement (string input instead of tokens, use for multiline code)
-//!
-//! See more examples in [tests/tests.rs](tests/tests.rs)
-//!
-//! ### Limitations
-//! Many. Most notably, `IDispatch` objects (i.e. what `CreateObject` returns) can't be passed to
-//! the engine (`let x = vbs! { CreateObject("WScript.Shell") }; vbs! { y = 'x }` won't work).
-//!
-//! ### Motivation
-//! N/A
-//!
-//! ### License
-//!
-//!
-//! This project is licensed under either of
-//!
-//!  * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
-//!    https://www.apache.org/licenses/LICENSE-2.0)
-//!  * MIT license ([LICENSE-MIT](LICENSE-MIT) or
-//!    https://opensource.org/licenses/MIT)
-//!
-//! at your option.
+#![doc = include_str!("../README.md")]
 
 use std::os::raw::c_char;
 pub use variant_rs::variant::ToVariant;
